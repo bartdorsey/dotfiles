1a11107fc24c2ca5f05e79d70b66b8f6f364eb5a
Bart Dorsey <bart@bartdorsey.com>
2022-10-04 16:05:04 -0500

    Updating aliases and zsh keybinds

--- .config/zsh/zsh-aliases.sh
+++ .config/zsh/zsh-aliases.sh
@@ -11,3 +11,5 @@ alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
 alias fgrep="fgrep --color=auto"
 alias ls="exa --icons --git -F -h"
+alias vim=nvim
+alias vi=nvim
--- .config/zsh/zsh-binds.sh
+++ .config/zsh/zsh-binds.sh
@@ -1,3 +1,5 @@
+bindkey "\e[H" beginning-of-line
+bindkey "\e[F" end-of-line
 bindkey -s '^o' 'fzf_vi^M'
 bindkey -s '^p' 'fzf_code^M'
 bindkey -s '^j' 'fzf_cd^M'

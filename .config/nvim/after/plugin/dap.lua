2c54c1f77d6294e1a2862d2139280189507fb816
Bart Dorsey <bart@bartdorsey.com>
2023-01-12 11:38:39 -0600

    updating neovim config

--- .config/nvim/after/plugin/dap.lua
+++ .config/nvim/after/plugin/dap.lua
@@ -8,5 +8,25 @@ vim.keymap.set("n", "<leader>dsi", dap.step_into)
 vim.keymap.set("n", "<leader>dsu", dap.step_out)
 vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
 
+dap.adapters.lldb = {
+  type = 'executable',
+  command = '/usr/bin/lldb-vscode-14', -- adjust as needed, must be absolute path
+  name = 'lldb'
+}
+
+-- Rust
+dap.configurations.rust = {{
+    name = 'Launch',
+    type = 'lldb',
+    request = 'launch',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+}};
+
+
 require('dapui').setup()
 

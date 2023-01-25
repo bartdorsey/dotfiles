244fb0d65f725b4a822da3b79d9e5aee6f921c51
Bart Dorsey <bart@bartdorsey.com>
2023-01-25 16:16:02 -0600

    Fixing .sh files

--- .config/zsh/plugins/20-nvm.zsh
+++ .config/zsh/plugins/20-nvm.zsh
@@ -1,8 +1,4 @@
-if [ -d "$HOME/.nvm" ]; then
+#export PYENV_ROOT="$HOME/.pyenv"
+#command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+#eval "$(pyenv init -)"
 
-    ### BEGIN INTIALIZE NVM
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion"
-    ### END INTIALIZE NVM
-fi

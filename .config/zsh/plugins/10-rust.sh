801d4c5edf3a3b0560019a2c27edc567cec40aa6
Bart Dorsey <bart@bartdorsey.com>
2023-01-25 12:47:30 -0600

    Fixing rust script

--- .config/zsh/plugins/10-rust.sh
+++ .config/zsh/plugins/10-rust.sh
@@ -1,5 +1,5 @@
-if [[ -d "${HOME}/.cargo" ]]; then
+if [[ ! -d "${HOME}/.cargo" ]]; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
-  export PATH=$PATH:$HOME/.cargo/bin
 fi
 
+export PATH=$PATH:$HOME/.cargo/bin

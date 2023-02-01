cb4503295c7847891905abd12d9e406bbb3ac07b
Bart Dorsey <bart@bartdorsey.com>
2023-02-01 16:54:37 -0600

    Fixing wget command

--- .config/zsh/plugins/95-nvim.zsh
+++ .config/zsh/plugins/95-nvim.zsh
@@ -1,6 +1,6 @@
 if ! type nvim > /dev/null; then
   TEMP_DEB="$(mktemp)" &&
-  wget -O https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb &&
+  wget -O "$TEMP_DEB" "https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb" &&
   sudo dpkg -i "$TEMP_DEB"
   rm -f "$TEMP_DEB"
 fi

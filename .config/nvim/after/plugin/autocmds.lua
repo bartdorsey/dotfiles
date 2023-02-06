2067891099f97714dffe044b1fcc3b237703e4ee
Bart Dorsey <bart@bartdorsey.com>
2023-02-06 14:29:23 -0600

    Disabling open nvim-tree on open

--- .config/nvim/after/plugin/autocmds.lua
+++ .config/nvim/after/plugin/autocmds.lua
@@ -32,4 +32,4 @@ local function open_nvim_tree(data)
   require("nvim-tree.api").tree.open()
 end
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })

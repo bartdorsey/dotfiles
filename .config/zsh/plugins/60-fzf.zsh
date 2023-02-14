2ec36d65ffec767c9f2972decc6cdbcd9e337b3d
Bart Dorsey <bart@bartdorsey.com>
2023-02-14 12:38:32 -0600

    Fixing fzf setup

--- .config/zsh/plugins/60-fzf.zsh
+++ .config/zsh/plugins/60-fzf.zsh
@@ -1,7 +1,9 @@
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
 if ! type fzf > /dev/null; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
+  [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 fi
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 

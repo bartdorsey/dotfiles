54df91c87195c80a4db4f2949d0ac63e252a2b47
Bart Dorsey <bart@bartdorsey.com>
2023-02-16 10:15:49 -0600

    Adding ability to generate the .grc-rs.zsh file if it doesn't exist

--- .config/zsh/plugins/96-grc-rs.zsh
+++ .config/zsh/plugins/96-grc-rs.zsh
@@ -1,7 +1,8 @@
 if type grc-rs > /dev/null; then
-    if [[ -e $ZDOTDIR/.grc-rs.zsh ]]; then
-        source $ZDOTDIR/.grc-rs.zsh
+    if [[ ! -e $ZDOTDIR/.grc-rs.zsh ]]; then
+        grc-rs --aliases > $ZDOTDIR/.grc-rs.zsh 
     fi
+    source $ZDOTDIR/.grc-rs.zsh
 else
     echo "grc-rs missing."
 fi

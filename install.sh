30c2b8a84f6194ff566345e326e3dee581b2a56b
Bart Dorsey <bart@bartdorsey.com>
2023-02-17 12:51:51 -0600

    Trying to make this a zsh plugin

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
-#!/bin/sh
+#!/bin/zsh
+#
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+PLUGIN_DIR=${0:h}
+
 # ZSH
-ln -sfv "$(pwd)/.zshenv" "$HOME/.zshenv"
+ln -sfv "${PLUGIN_DIR}/.zshenv" "$HOME/.zshenv"
 # .config folder
-ln -sfv "$(pwd)/.config/"* "$HOME/.config/"
+ln -sfv "${PLUGIN_DIR}/.config/"* "$HOME/.config/"
 # tmux
-ln -sfv "$(pwd)/.tmux.conf" "$HOME/.tmux.conf"
-ln -sfv "$(pwd)/.tmux/"* "$HOME/.tmux/"
+ln -sfv "${PLUGIN_DIR}/.tmux.conf" "$HOME/.tmux.conf"
+ln -sfv "${PLUGIN_DIR}/.tmux/"* "$HOME/.tmux/"
 # SSH Setup
 if [ ! -d "$HOME/.ssh" ]; then
     mkdir "$HOME/.ssh"
 fi
-ln -sfv "$(pwd)/.ssh/config" "$HOME/.ssh/config"
+ln -sfv "${PLUGIN_DIR}/.ssh/config" "$HOME/.ssh/config"
 

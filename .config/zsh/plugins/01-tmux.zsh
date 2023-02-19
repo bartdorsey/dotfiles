0a1c04d9b0af9506c6da532d4350a0231c17fb44
Bart Dorsey <bart@bartdorsey.com>
2023-02-19 13:03:51 -0600

    Updating for macOS with tmux terminfo hack

--- .config/zsh/plugins/01-tmux.zsh
+++ .config/zsh/plugins/01-tmux.zsh
@@ -1,3 +1,8 @@
+if [[ $(uname -s) == "Darwin" ]]; then
+   /usr/bin/tic -x -o $HOME/.local/share/terminfo $HOME/.dotfiles/terminfo/tmux-256color.info 
+   export TERMINFO_DIRS=$TERMINFO_DIRS:$HOME/.local/share/terminfo
+fi
+
 if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi

a597659011cdb3a1ff36658262c845654cbdf223
Bart Dorsey <bart@bartdorsey.com>
2023-02-22 15:17:03 -0600

    Adding nix

---
+++ .config/zsh/plugins/00-nix.zsh
@@ -0,0 +1,22 @@
+if [[ ! type nix-env > /dev/null ]]; then 
+   sh <(curl -L https://nixos.org/nix/install) --daemon
+fi
+
+if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then 
+   . $HOME/.nix-profile/etc/profile.d/nix.sh;
+fi # added by Nix installer
+
+# Install tmux
+if [[ ! type tmux > /dev/null ]]; then
+   nix-env -iA nixpkgs.tmux
+fi
+
+# Install neovim
+if [[ ! type nvim > /dev/null ]]; then
+   nix-env -iA nixpkgs.neovim
+fi
+
+# Install starship
+if [[ ! type starship > /dev/null ]]; then
+   nix-env -iA nixpkgs.starship
+fi

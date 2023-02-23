86fb69b6095ee8006463086fb0f5d59dea9d9353
Bart Dorsey <bart@bartdorsey.com>
2023-02-23 16:14:25 -0600

    Running all profile.d files for nix

--- .config/zsh/plugins/00-nix.zsh
+++ .config/zsh/plugins/00-nix.zsh
@@ -2,7 +2,10 @@ if [ ! -e /nix ]; then
    sh <(curl -L https://nixos.org/nix/install) --daemon
 fi
 
-if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then 
-   . $HOME/.nix-profile/etc/profile.d/nix.sh;
-fi # added by Nix installer
-
+if [ -d $HOME/.nix-profile/etc/profile.d ] ; then
+   for i in $HOME/.nix-profile/etc/profile.d/*.sh; do
+      if [ -r $i ]; then
+         . $i
+      fi
+   done
+fi

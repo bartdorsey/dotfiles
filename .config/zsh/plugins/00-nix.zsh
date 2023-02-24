c62ad03d668eba5d2627e8ec2740c73608e11f11
Bart Dorsey <bart@bartdorsey.com>
2023-02-23 18:18:35 -0600

    Hacking grc to work

--- .config/zsh/plugins/00-nix.zsh
+++ .config/zsh/plugins/00-nix.zsh
@@ -2,6 +2,8 @@ if [ ! -e /nix ]; then
    sh <(curl -L https://nixos.org/nix/install) --daemon
 fi
 
+export GRC_ALIASES=true
+
 if [ -d $HOME/.nix-profile/etc/profile.d ] ; then
    for i in $HOME/.nix-profile/etc/profile.d/*.sh; do
       if [ -r $i ]; then

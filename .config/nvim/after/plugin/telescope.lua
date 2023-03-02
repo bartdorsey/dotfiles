9f7ec37eaff46155a94608f9f3091d10d780342e
Bart Dorsey <bart@bartdorsey.com>
2023-03-02 09:20:04 -0600

    adding file browser mappings

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -19,6 +19,7 @@ require('telescope').setup {
 
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
+pcall(require('telescope').load_extension, 'file_browser')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
@@ -31,11 +32,12 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set("n", "<leader>cp", require('telescope.builtin').commands, { desc = "[C]ommand [P]allet" })
-vim.keymap.set("n", "<leader>sg", require('telescope.builtin').git_files, { desc = "Search [G]it [F]iles" })
-vim.keymap.set("n", "<leader>sk", require('telescope.builtin').keymaps, { desc = "[S]earch [K]eymaps" })
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[s]earch [f]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[s]earch [h]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[s]earch current [w]ord' })
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').live_grep, { desc = '[s]earch by [g]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[s]earch [d]iagnostics' })
+vim.keymap.set("n", "<leader>cp", require('telescope.builtin').commands, { desc = "[c]ommand [p]allet" })
+vim.keymap.set("n", "<leader>sg", require('telescope.builtin').git_files, { desc = "Search [g]it [f]iles" })
+vim.keymap.set("n", "<leader>sk", require('telescope.builtin').keymaps, { desc = "[s]earch [k]eymaps" })
+vim.keymap.set("n", "<leader>fb", require('telescope').extensions.file_browser.file_browser, { desc = "[f]ile [b]rowser" })

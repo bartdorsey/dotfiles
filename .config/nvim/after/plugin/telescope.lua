9300460f8c5487e07ac12eba7c6aa945cfc4e911
Bart Dorsey <bart@bartdorsey.com>
2023-03-02 09:07:02 -0600

    Supposed to be a table of lua patterns

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,7 +2,7 @@
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
-    file_ignore_patterns = "^.git/",
+    file_ignore_patterns = { "^.git/" },
     mappings = {
       i = {
         ['<C-u>'] = false,

64a673378c780b36ca7ea3b6c21b622c97b64376
Bart Dorsey <bart@bartdorsey.com>
2023-03-02 16:01:32 -0600

    Updating fzf script to read from nix

--- .config/zsh/plugins/60-fzf.zsh
+++ .config/zsh/plugins/60-fzf.zsh
@@ -1,4 +1,7 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+
+[ -f ~/.nix-profile/share/fzf/completion.zsh ] && source ~/.nix-profile/share/fzf/completion.zsh
+[ -f ~/.nix-profile/share/fzf/key-bindings.zsh ] && source ~/.nix-profile/share/fzf/key-bindings.zsh
 
 if ! type fzf > /dev/null; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf

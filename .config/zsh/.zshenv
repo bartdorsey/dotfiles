22db766cb0269f9379a4553d4004c37abddac84b
Bart Dorsey <bart@bartdorsey.com>
2023-03-02 18:24:29 -0600

    Tryign to fix grc

--- .config/zsh/.zshenv
+++
@@ -1,2 +0,0 @@
-if [ -e /home/echo/.nix-profile/etc/profile.d/nix.sh ]; then . /home/echo/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
-
--- .config/zsh/plugins/00-nix.zsh
+++ .config/zsh/plugins/00-nix.zsh
@@ -4,8 +4,13 @@ fi
 
 export GRC_ALIASES=true
 
+if [ -e $HOME/.nix-profile/etc/profile.d/grc.sh ]; then
+   . $HOME/.nix-profile/etc/profile.d/grc.sh
+fi
+
 if [ -d $HOME/.nix-profile/etc/profile.d ] ; then
    for i in $HOME/.nix-profile/etc/profile.d/*.sh; do
+      echo $i
       if [ -r $i ]; then
          . $i
       fi

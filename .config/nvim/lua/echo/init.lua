2f7ccc95d5bbd31f117255dfae5cfc0ea1295ec2
Bart Dorsey <bart@bartdorsey.com>
2023-03-05 13:03:11 -0600

    Moving config files into main lua directory

--- .config/nvim/lua/echo/init.lua
+++ .config/nvim/lua/echo/init.lua
@@ -1,3 +1,6 @@
-require('echo.remap');
+require('echo.neovide');
+require('echo.set');
+require('echo.keymaps');
 require('echo.plugins');
+require('echo.autocmds')
 
--- .config/nvim/lua/echo/keymaps.lua
+++ .config/nvim/lua/echo/keymaps.lua
@@ -22,3 +22,29 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
+vim.api.nvim_set_keymap('n', '<Leader>mp', ':MarkdownPreview<CR>', { silent = true })
+
+-- Let you move visual blocks with J and K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Cursor in middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Yank into system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Captial Q is the worst place in the universe
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Control-S save
+vim.keymap.set({"n", "i", "v"}, "<C-s>", "<cmd>w<CR>")
+

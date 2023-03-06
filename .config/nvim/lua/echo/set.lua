f532a7d3244467cd32d8d1fe529ba5b1e678be7d
Bart Dorsey <bart@bartdorsey.com>
2023-03-06 00:06:35 -0600

    Adding mini animate and setting default spacing to 2

--- .config/nvim/lua/echo/set.lua
+++ .config/nvim/lua/echo/set.lua
@@ -26,8 +26,8 @@ vim.wo.signcolumn = 'yes'
 vim.o.completeopt = 'menuone,noselect,preview'
 
 -- Indenting
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 

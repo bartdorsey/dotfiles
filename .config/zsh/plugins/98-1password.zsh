55a43a6b04273905611a905f6b5870a60975afe4
Bart Dorsey <bart@bartdorsey.com>
2023-03-10 15:37:46 -0600

    Making alias a function

--- .config/zsh/plugins/98-1password.zsh
+++ .config/zsh/plugins/98-1password.zsh
@@ -1,3 +1,5 @@
 if type op > /dev/null; then
-   alias 1p=op item get $(echo op item list | fzf | cut -f 1 -d " ")
+   function 1p {
+      op item get $(echo op item list | fzf | cut -f 1 -d " ")
+   }
 fi

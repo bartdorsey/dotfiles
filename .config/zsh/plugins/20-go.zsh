57bcf9104714019b5009ab6b11c274e4b750177a
Bart Dorsey <bart@bartdorsey.com>
2023-03-14 14:14:59 -0500

    Fixing syntax error in go

--- .config/zsh/plugins/20-go.zsh
+++ .config/zsh/plugins/20-go.zsh
@@ -1,4 +1,4 @@
-if [-d "$HOME/.nix-profile/share/go" ]; then
+if [ -d "$HOME/.nix-profile/share/go" ]; then
     export PATH="$HOME/.nix-profile/share/go/bin/:$PATH"
     export GOROOT="$HOME/.nix-profile/share/go/"
 fi

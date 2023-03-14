d98e88f2f9aa9dd0df4d54a115fd89255d3926ad
Bart Dorsey <bart@bartdorsey.com>
2023-03-14 13:47:55 -0500

    fnm won't initialize if it's not installed

--- .config/zsh/plugins/20-fnm.zsh
+++ .config/zsh/plugins/20-fnm.zsh
@@ -1,5 +1,7 @@
-eval "$(fnm env --shell=zsh --use-on-cd)"
-
-if ! type node > /dev/null; then
-    fnm install --lts
+if type fnm > /dev/null; then
+    eval "$(fnm env --shell=zsh --use-on-cd)"
+    if ! type node > /dev/null; then
+        fnm install --lts
+    fi
 fi
+

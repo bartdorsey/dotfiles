548107cee8e6c61bfb0ecb6e25653762a9847bc2
Bart Dorsey <bart@bartdorsey.com>
2023-03-15 17:08:27 -0500

    adding undotrre

---
+++ .config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,19 @@
+return {
+    "mbbill/undotree",
+    keys = {
+        {
+            '<leader>u',
+            '<cmd>UndotreeToggle<cr>',
+            desc = '[u]ndotree'
+        }
+    },
+    config = function()
+        vim.g.undotree_WindowLayout = 2
+        vim.g.undotree_SplitWidth = 40
+        vim.g.undotree_TreeNodeShape = '●'
+        vim.g.undotree_TreeVertShape = '│'
+        vim.g.undotree_TreeSplitShape = '╱'
+        vim.g.undotree_TreeReturnShape = '╲'
+        vim.g.undotree_DiffCommand = "diff -u"
+    end
+}

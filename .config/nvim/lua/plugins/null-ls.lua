016d5e63a845cf31c0f22f87a644689d53fc7e39
Bart Dorsey <bart@bartdorsey.com>
2023-03-16 14:51:06 -0500

    updating null ls

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -8,9 +8,21 @@ return {
             sources = {
                 null_ls.builtins.formatting.markdownlint,
                 null_ls.builtins.diagnostics.markdownlint,
-                null_ls.builtins.completion.cspell,
-                null_ls.builtins.diagnostics.alex,
-                null_ls.builtins.hover.dictionary,
+                null_ls.builtins.diagnostics.cspell.with({
+                    filetypes = { 'markdown' }
+                }),
+                null_ls.builtins.code_actions.cspell.with({
+                    filetypes = { 'markdown' }
+                }),
+                null_ls.builtins.completion.spell.with({
+                    filetypes = { 'markdown' }
+                }),
+                null_ls.builtins.diagnostics.alex.with({
+                    filetypes = { 'markdown' }
+                }),
+                null_ls.builtins.hover.dictionary.with({
+                    filetypes = { 'markdown' }
+                }),
                 null_ls.builtins.formatting.prettierd.with({
                     filetypes = { "html", "json", "yaml", "markdown" }
                 })

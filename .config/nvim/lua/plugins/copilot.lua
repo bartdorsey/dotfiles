c79d93ab0054b10ab5b4490448267238bc992956
Bart Dorsey <bart@bartdorsey.com>
2023-03-27 12:00:35 -0500

    Updating to copilot.lua

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,21 @@
 return {
-    'github/copilot.vim',
+    --'github/copilot.vim',
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        config = function()
+            require("copilot").setup({
+                suggestion = { enabled = false },
+                panel = { enabled = false },
+            })
+        end,
+    },
+    {
+        "zbirenbaum/copilot-cmp",
+        lazy = true,
+        dependencies = { "zbirenbaum/copilot.lua" },
+        config = function()
+            require("copilot_cmp").setup()
+        end,
+    },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -19,6 +19,8 @@ return {
         { "saadparwaiz1/cmp_luasnip" },
         { "hrsh7th/cmp-nvim-lsp" },
         { "hrsh7th/cmp-nvim-lua" },
+        { "onsails/lspkind-nvim" },
+        { "zbirenbaum/copilot-cmp" },
 
         -- Snippets
         { "L3MON4D3/LuaSnip" },
@@ -28,7 +30,7 @@ return {
             "folke/neodev.nvim",
             ft = "lua",
             config = function()
-                require('neodev').setup()
+                require("neodev").setup()
             end,
         },
     },
@@ -320,6 +322,23 @@ return {
         lsp.setup_nvim_cmp({
             preselect = "none",
             mapping = cmp_mappings,
+            sources = {
+                { name = "path" },
+                { name = "nvim_lsp" },
+                { name = "buffer", keyword_length = 3 },
+                { name = "luasnip", keyword_length = 2 },
+                { name = "copilot", group_index = 2 },
+            },
+            formatting = {
+                format = require("lspkind").cmp_format({
+                    mode = "symbol_text",
+                    maxwidth = 50,
+                    ellipsis_char = "...",
+                    symbol_map = {
+                        Copilot = "",
+                    },
+                }),
+            },
             completion = {
                 completeopt = "menu,menuone,noinsert,noselect",
             },

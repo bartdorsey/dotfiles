ee3a4fb1debf47186b4501e217fcbd0fe5e48880
Bart Dorsey <bart@bartdorsey.com>
2023-03-27 13:57:16 -0500

    Updating theme

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,35 +1,58 @@
-function ColorMyPencils(color)
-  color = color or "catppuccin"
-  vim.cmd.colorscheme(color)
-
-  -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 -- Theme
 return {
-  -- StatusLine
   {
-    'catppuccin/nvim',
+    "catppuccin/nvim",
     name = "catppuccin",
     lazy = false,
     priority = 1000,
     config = function()
-      ColorMyPencils()
-      require('catppuccin').setup({
+      vim.cmd.colorscheme('catppuccin')
+      require("catppuccin").setup({
+        integrations = {
+          lsp_trouble = true,
+          markdown = true,
+          dap = {
+            enabled = true,
+            enable_ui = true
+          },
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+            }
+          },
+          harpoon = true,
+          neotree = true,
+          gitsigns = true,
+          ts_rainbow2 = true,
+          which_key = true,
+          notify = true,
+          noice = true
+        }
       })
-    end
+    end,
   },
   {
-    'feline-nvim/feline.nvim',
-    dependencies = { 'catppuccin/nvim' },
+    "feline-nvim/feline.nvim",
+    dependencies = { "catppuccin/nvim" },
     config = function()
       require("feline").setup({
         components = require("catppuccin.groups.integrations.feline").get(),
       })
-    end
+      require("feline").winbar.setup()
+    end,
   },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
 -- vim: ts=2 sts=2 sw=2 et

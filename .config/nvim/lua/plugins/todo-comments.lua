eab6560acafff83d5550b21df5dbe845f3e038b7
Bart Dorsey <bart@bartdorsey.com>
2023-03-27 12:00:21 -0500

    Making todo load lazy

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,6 +1,7 @@
 return {
     'folke/todo-comments.nvim',
     dependencies = { "nvim-lua/plenary.nvim"},
+    cmd = {"TodoTrouble", "TodoTelescope"},
     config = function()
         require('todo-comments').setup()
     end

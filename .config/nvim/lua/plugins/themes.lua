3ee1b8db7c17af35b7c619fee27a44d39dccbcb3
Bart Dorsey <bart@bartdorsey.com>
2023-03-28 08:51:07 -0500

    Updating catppuccin theme

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,58 +1,54 @@
 -- Theme
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme('catppuccin')
-      require("catppuccin").setup({
-        integrations = {
-          lsp_trouble = true,
-          markdown = true,
-          dap = {
-            enabled = true,
-            enable_ui = true
-          },
-          native_lsp = {
-            enabled = true,
-            virtual_text = {
-              errors = { "italic" },
-              hints = { "italic" },
-              warnings = { "italic" },
-              information = { "italic" },
-            },
-            underlines = {
-              errors = { "underline" },
-              hints = { "underline" },
-              warnings = { "underline" },
-              information = { "underline" },
-            }
-          },
-          harpoon = true,
-          neotree = true,
-          gitsigns = true,
-          ts_rainbow2 = true,
-          which_key = true,
-          notify = true,
-          noice = true
-        }
-      })
-    end,
-  },
-  {
-    "feline-nvim/feline.nvim",
-    dependencies = { "catppuccin/nvim" },
-    config = function()
-      require("feline").setup({
-        components = require("catppuccin.groups.integrations.feline").get(),
-      })
-      require("feline").winbar.setup()
-    end,
-  },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("catppuccin")
+            require("catppuccin").setup({
+                integrations = {
+                    lsp_trouble = true,
+                    markdown = true,
+                    dap = {
+                        enabled = true,
+                        enable_ui = true,
+                    },
+                    native_lsp = {
+                        enabled = true,
+                        virtual_text = {
+                            errors = { "italic" },
+                            hints = { "italic" },
+                            warnings = { "italic" },
+                            information = { "italic" },
+                        },
+                        underlines = {
+                            errors = { "underline" },
+                            hints = { "underline" },
+                            warnings = { "underline" },
+                            information = { "underline" },
+                        },
+                    },
+                    harpoon = true,
+                    neotree = true,
+                    gitsigns = true,
+                    ts_rainbow2 = true,
+                    which_key = true,
+                    notify = true,
+                    noice = true,
+                },
+            })
+        end,
+    },
+    {
+        "feline-nvim/feline.nvim",
+        dependencies = { "catppuccin/nvim" },
+        config = function()
+            require("feline").setup({
+                components = require("catppuccin.groups.integrations.feline").get(),
+            })
+            require("feline").winbar.setup()
+        end,
+    },
 }
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
--- vim: ts=2 sts=2 sw=2 et

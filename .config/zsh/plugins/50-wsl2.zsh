ab649ec03ba9af2aa1511136248f648004fd4ba9
Bart Dorsey <bart@bartdorsey.com>
2023-03-30 11:11:29 -0500

    Updating wsl setup file

--- .config/zsh/plugins/50-wsl2.zsh
+++ .config/zsh/plugins/50-wsl2.zsh
@@ -1,37 +1,39 @@
 if [[ -v WSL_DISTRO_NAME ]]; then
-   # Configure ssh forwarding
-   #export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-   # need `ps -ww` to get non-truncated command for matching
-   # use square brackets to generate a regex match for the process we want but that doesn't match the grep command running it!
-   #ALREADY_RUNNING=$(ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $?)
-   #if [[ $ALREADY_RUNNING != "0" ]]; then
-   #   if [[ -S $SSH_AUTH_SOCK ]]; then
-   #      # not expecting the socket to exist as the forwarding command isn't running (http://www.tldp.org/LDP/abs/html/fto.html)
-   #      echo "removing previous socket..."
-   #      rm $SSH_AUTH_SOCK
-   #   fi
-   #   echo "Starting SSH-Agent relay..."
-   #   # setsid to force new session to keep running
-   #   # set socat to listen on $SSH_AUTH_SOCK and forward to npiperelay which then forwards to openssh-ssh-agent on windows
-   #   (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
-   #fi
+    # Configure ssh forwarding
+    #export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+    # need `ps -ww` to get non-truncated command for matching
+    # use square brackets to generate a regex match for the process we want but that doesn't match the grep command running it!
+    #ALREADY_RUNNING=$(ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $?)
+    #if [[ $ALREADY_RUNNING != "0" ]]; then
+    #   if [[ -S $SSH_AUTH_SOCK ]]; then
+    #      # not expecting the socket to exist as the forwarding command isn't running (http://www.tldp.org/LDP/abs/html/fto.html)
+    #      echo "removing previous socket..."
+    #      rm $SSH_AUTH_SOCK
+    #   fi
+    #   echo "Starting SSH-Agent relay..."
+    #   # setsid to force new session to keep running
+    #   # set socat to listen on $SSH_AUTH_SOCK and forward to npiperelay which then forwards to openssh-ssh-agent on windows
+    #   (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+    #fi
 
-   export WSL_INTEROP=
-   for socket in /run/WSL/*; do
-      if ss -elx | grep -q "$socket"; then
-         export WSL_INTEROP=$socket
-      else
-         rm -v $socket 
-      fi
-   done
+    export WSL_INTEROP=
+    for socket in /run/WSL/*; do
+        if ss -elx | grep -q "$socket"; then
+            export WSL_INTEROP=$socket
+        else
+            rm -v $socket
+        fi
+    done
 
-   if [[ -z $WSL_INTEROP ]]; then
-      echo -e "\033[31mNo working WSL_INTEROP socket found !\033[0m" 
-   fi
+    if [[ -z $WSL_INTEROP ]]; then
+        echo -e "\033[31mNo working WSL_INTEROP socket found !\033[0m"
+    fi
 
-   # WSL aliases
-   alias clip='clip.exe'
-   alias open='explorer.exe'
-   alias wsl='wsl.exe'
-   alias pwsh='pwsh.exe'
+    # WSL aliases
+    alias clip='clip.exe'
+    alias open='explorer.exe'
+    alias wsl='wsl.exe'
+    alias pwsh='pwsh.exe'
+
+    notify-send() { wsl-notify-send.exe --category $WSL_DISTRO_NAME "${@}"; }
 fi

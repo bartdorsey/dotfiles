087508ed6c38fdc0d174e4b25e194bbfa583c69c
Bart Dorsey <bart@bartdorsey.com>
2023-04-11 09:06:29 -0500

    Adding clone all script

---
+++ clone-all.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+grm repos sync remote --provider github --access --token-command "pass show github_token" --root ~/github --worktree --force-ssh
+grm repos sync remote --provider gitlab --access --token-command "pass show github_token" --root ~/github --worktree --force-ssh

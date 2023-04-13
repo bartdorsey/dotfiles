669f0b003a9e0a9332a515e8a115e5d3b2c1960d
Bart Dorsey <bart@bartdorsey.com>
2023-04-13 10:02:35 -0500

    Adding exclude dirs for zoxide

--- zsh/.config/zsh/plugins/60-zoxide.zsh
+++ zsh/.config/zsh/plugins/60-zoxide.zsh
@@ -1,4 +1,6 @@
 if type zoxide > /dev/null; then
+    # Set exlude dirs for zoxide
+    export _ZO_EXCLUDE_DIRS="$HOME/.cache/*"
     # Initialize zoxide
     eval "$(zoxide init zsh)"
     alias j=z

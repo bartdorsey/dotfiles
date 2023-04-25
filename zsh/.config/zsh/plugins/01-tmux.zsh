3b60b93aa2f26b78aa59a56f1d7b3fbb32ee65e4
Bart Dorsey <bart@bartdorsey.com>
2023-04-25 16:29:47 -0500

    updating tmux config

--- zsh/.config/zsh/plugins/01-tmux.zsh
+++ zsh/.config/zsh/plugins/01-tmux.zsh
@@ -1,4 +1,3 @@
-if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
-    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+if [[ ! -d "$HOME/.config/tmux/plugins/tpm" ]]; then
+    git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
 fi
-

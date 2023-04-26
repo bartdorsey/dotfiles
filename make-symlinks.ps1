042f17e27ca665383459c403ae22ebdd1d2c3983
Bart Dorsey <bart@bartdorsey.com>
2023-04-26 10:42:39 -0500

    Adding symlink for AppData for nvim

--- make-symlinks.ps1
+++ make-symlinks.ps1
@@ -3,6 +3,7 @@ function New-Link ($target, $link) {
 }
 
 New-Link $HOME/.dotfiles/nvim/.config/nvim $HOME/.config/nvim
+New-Link $HOME/.dotfiles/nvim/.config/nvim $HOME/AppData/Local/nvim
 New-Link $HOME/.dotfiles/starship/.config/starship.toml $HOME/.config/starship.toml
 New-Link $HOME/.dotfiles/Microsoft.PowerShell_profile.ps1 $PROFILE
 New-Link $HOME/.dotfiles/ssh/.ssh/config $HOME/.ssh/config

ce5d8f8789c191ea3511c54105d9e96b5c69cfd8
Bart Dorsey <bart@bartdorsey.com>
2023-05-03 17:22:09 -0500

    Fixing vim aliases

--- zsh/.config/zsh/plugins/95-nvim.zsh
+++ zsh/.config/zsh/plugins/95-nvim.zsh
@@ -1,6 +1,5 @@
 if type nvim > /dev/null;then
-    alias ovim=vim
-    alias vi=vim
-    alias vim=nvim
+    alias vi=$(which vim)
+    alias vim=$(which nvim)
     alias nvchad="NVIM_APPNAME=nvchad nvim"
 fi

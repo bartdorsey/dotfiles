1049a744ade56a302db580ce9bed99d1bf45c9eb
Bart Dorsey <bart@bartdorsey.com>
2023-05-28 15:22:34 -0500

    Updating gopath

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -47,6 +47,7 @@ export PASSWORD_STORE_DIR="${XDG_DATA_HOME}/pass"
 
 # Go
 export GOPATH="${XDG_DATA_HOME}/go"
+export PATH=$PATH:$GOPATH/bin
 
 # wget
 alias wget="wget --hsts-file="${XDG_CACHE_HOME}/wget-hsts""

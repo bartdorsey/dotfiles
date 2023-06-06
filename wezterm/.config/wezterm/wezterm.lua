75356b02a57024629122bdf544a89f853b2b8327
Bart Dorsey <bart@bartdorsey.com>
2023-06-06 18:36:51 -0500

    Adding wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
+	font = wezterm.font("IosevkaTerm Nerd Font"),
+	font_size = 14,
+	enable_tab_bar = true,
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = true,
+	window_background_opacity = 0.9,
+}

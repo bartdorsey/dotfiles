9db32173e8ac9de6c9a627b6bb1d6f9a7087b116
Bart Dorsey <bart@bartdorsey.com>
2023-06-25 00:31:17 -0500

    Font size 14 for wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -31,7 +31,7 @@ end)
 return {
     color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
     font = wezterm.font("IosevkaTerm Nerd Font"),
-    font_size = 16,
+    font_size = 14,
     enable_tab_bar = true,
     use_fancy_tab_bar = false,
     tab_bar_at_bottom = true,

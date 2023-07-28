6bb18880964bd4f097380216f1907ced0bdaaa07
Bart Dorsey <bart@bartdorsey.com>
2023-07-28 01:02:49 -0500

    Updating oil.nvim

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -4,8 +4,30 @@ return {
     config = function()
         require("oil").setup({
             default_file_explorer = true,
+            skip_confim_for_simple_edits = true,
+            keymaps = {
+                ["g?"] = "actions.show_help",
+                ["<CR>"] = "actions.select",
+                ["<C-s>"] = "actions.select_vsplit",
+                ["<C-h>"] = "actions.select_split",
+                ["<C-t>"] = "actions.select_tab",
+                ["<C-p>"] = "actions.preview",
+                ["<C-c>"] = "actions.close",
+                ["<C-l>"] = "actions.refresh",
+                ["-"] = "actions.parent",
+                ["_"] = "actions.open_cwd",
+                ["`"] = "actions.cd",
+                ["~"] = "actions.tcd",
+                ["g."] = "actions.toggle_hidden",
+                ["<esc>"] = "actions.close"
+            },
             columns = {
                 "icon",
+            },
+            float = {
+                padding = 4,
+                max_width = 80,
+                max_height = 25
             },
             view_options = {
                 show_hidden = true,

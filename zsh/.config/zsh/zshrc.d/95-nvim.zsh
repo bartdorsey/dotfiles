1733413e6272691c5284f6af959c7e8f2203630f
Bart Dorsey <bart@bartdorsey.com>
2023-07-28 01:02:34 -0500

    Updating nvchad

--- zsh/.config/zsh/zshrc.d/95-nvim.zsh
+++ zsh/.config/zsh/zshrc.d/95-nvim.zsh
@@ -2,5 +2,5 @@ alias vi=$(which vim)
 
 if type nvim > /dev/null;then
     alias vim=$(which nvim)
-    alias nvchad="NVIM_APPNAME=nvchad nvim"
+    alias nvchad="VIMINIT= NVIM_APPNAME=nvchad nvim"
 fi

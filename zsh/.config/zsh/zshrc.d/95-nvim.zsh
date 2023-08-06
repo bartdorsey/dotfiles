38b7a8eeef0aade31172ff77da8fec0428d40313
Bart Dorsey <bart@bartdorsey.com>
2023-08-06 18:01:28 -0500

    Updating to add butss

--- zsh/.config/zsh/zshrc.d/95-nvim.zsh
+++ zsh/.config/zsh/zshrc.d/95-nvim.zsh
@@ -3,4 +3,5 @@ alias vi=$(which vim)
 if type nvim > /dev/null;then
     alias vim=$(which nvim)
     alias nvchad="VIMINIT= NVIM_APPNAME=nvchad nvim"
+    alias butts="VIMINIT= NVIM_APPNAME=alphakeks nvim"
 fi

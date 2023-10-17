6c69859c6b231995f9a6f23f31340a1d38180b00
Bart Dorsey <bart@bartdorsey.com>
2023-10-17 16:37:27 -0500

    Fixing macchine to work in regular terminals but not in VSCode

--- zsh/.config/zsh/macchina.zsh
+++ zsh/.config/zsh/macchina.zsh
@@ -1,3 +1,6 @@
-if type macchina > /dev/null; then
-    macchina -t Catppuccin
+if [[ "$TERM_PROGRAM" != "vscode" ]]; then
+    if type macchina > /dev/null; then
+        macchina -t Catppuccin
+    fi
 fi
+

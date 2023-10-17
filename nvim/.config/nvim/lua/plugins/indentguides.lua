0bc9f6fef8021e4edf069fbac44baa9314315e76
Bart Dorsey <bart@bartdorsey.com>
2023-10-17 22:13:22 +0000

    updating indent guides config

--- nvim/.config/nvim/lua/plugins/indentguides.lua
+++ nvim/.config/nvim/lua/plugins/indentguides.lua
@@ -4,9 +4,15 @@ return {
     'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
     event = "BufReadPre",
     config = function()
-        require('indent_blankline').setup {
-            char = '┊',
-            show_trailing_blankline_indent = false
-        }
+        require('ibl').setup({
+            indent = { char = '┊' },
+            whitespace = {
+                remove_blankline_trail = false
+            },
+            scope = {
+                enabled = true,
+                show_exact_scope = true
+            }
+        })
     end
 }

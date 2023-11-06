d014b366a607e913505f6a7b541455b83dcb7809
Bart Dorsey <bart@bartdorsey.com>
2023-11-06 13:40:09 -0600

    Adding borders

--- nvim/.config/nvim/lua/plugins/lsp/onattach.lua
+++ nvim/.config/nvim/lua/plugins/lsp/onattach.lua
@@ -122,7 +122,20 @@ return function(args)
         end
     end, { desc = "Toggle Line Diagnostics" })
 
+    local _border = "single"
+
+    vim.lsp.handlers["textDocument/hover"] =
+        vim.lsp.with(vim.lsp.handlers.hover, {
+            border = _border,
+        })
+
+    vim.lsp.handlers["textDocument/signatureHelp"] =
+        vim.lsp.with(vim.lsp.handlers.signature_help, {
+            border = _border,
+        })
+
     vim.diagnostic.config({
+        float = { border = _border },
         virtual_text = {
             source = true,
         },

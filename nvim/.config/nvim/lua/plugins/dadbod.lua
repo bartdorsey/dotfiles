4ee9f1ebebcc1c8e3479e3bc93a32fe8cd673439
Bart Dorsey <bart@bartdorsey.com>
2023-11-18 18:17:03 -0600

    Adding dadbod

--- nvim/.config/nvim/lua/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/plugins/dadbod.lua
@@ -1,4 +1,21 @@
 return {
-    "tpope/vim-dadbod",
-    config = function() end,
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+        { "tpope/vim-dadbod", lazy = true },
+        {
+            "kristijanhusak/vim-dadbod-completion",
+            ft = { "sql", "mysql", "plsql" },
+            lazy = true,
+        },
+    },
+    cmd = {
+        "DBUI",
+        "DBUIToggle",
+        "DBUIAddConnection",
+        "DBUIFindBuffer",
+    },
+    init = function()
+        -- Your DBUI configuration
+        vim.g.db_ui_use_nerd_fonts = 1
+    end,
 }

beaffcbd3e0a08abd924421ecac837d5fb2509e1
Bart Dorsey <bart@bartdorsey.com>
2023-12-11 17:49:42 -0600

    Adding copilot insertenter event

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,8 +1,9 @@
 return {
-    --'github/copilot.vim',
+    -- "github/copilot.vim",
     {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
+        event = "InsertEnter",
         config = function()
             require("copilot").setup({
                 suggestion = { enabled = false },

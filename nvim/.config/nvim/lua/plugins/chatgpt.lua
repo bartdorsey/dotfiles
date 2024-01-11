4395823a8ab01ac8ee20f5375872bdc160ab99c0
Bart Dorsey <bart@bartdorsey.com>
2024-01-11 13:54:13 -0600

    Adding chatGPT plugin

---
+++ nvim/.config/nvim/lua/plugins/chatgpt.lua
@@ -0,0 +1,14 @@
+return {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+        require("chatgpt").setup({
+            api_key_cmd = "pass openai",
+        })
+    end,
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+}

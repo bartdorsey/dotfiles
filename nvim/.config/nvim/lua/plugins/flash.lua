e6030940a656fccc575362e1a9c66d223cc3a6ed
Bart Dorsey <bart@bartdorsey.com>
2024-01-26 10:38:56 -0600

    Changing flash config slightly

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -18,20 +18,12 @@ return {
     },
     keys = {
         {
-            "s",
-            mode = { "n", "x", "o" },
-            function()
-                require("flash").jump()
-            end,
-            desc = "Flash",
-        },
-        {
             "S",
             mode = { "n", "x", "o" },
             function()
-                require("flash").treesitter()
+                require("flash").treesitter_search()
             end,
-            desc = "Flash Treesitter",
+            desc = "Flash Treesitter Select",
         },
     },
 }

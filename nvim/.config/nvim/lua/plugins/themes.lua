9ec04235798a56aeaa54230a2abdee39c60e0b49
Bart Dorsey <bart@bartdorsey.com>
2024-01-29 12:57:54 -0600

    Updating theme colors for rainbow delimiters

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -3,15 +3,37 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         dependencies = {
-            "RRethy/nvim-base16",
             "rose-pine/neovim",
         },
         config = function()
-            require("base16-colorscheme").with_config({
-                telescope_borders = true,
-            })
-
             vim.cmd("colorscheme rose-pine")
+
+            local colors = require("rose-pine.palette")
+
+            -- rainbow delimiters
+            vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = colors.rose })
+            vim.api.nvim_set_hl(
+                0,
+                "RainbowDelimiterYellow",
+                { fg = colors.gold }
+            )
+            vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = colors.foam })
+            vim.api.nvim_set_hl(
+                0,
+                "RainbowDelimiterGreen",
+                { fg = colors.pine }
+            )
+            vim.api.nvim_set_hl(
+                0,
+                "RainbowDelimiterViolet",
+                { fg = colors.iris }
+            )
+            vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = colors.foam })
+            vim.api.nvim_set_hl(
+                0,
+                "RainbowDelimiterOrange",
+                { fg = colors.gold }
+            )
 
             -- Custom LuaLine statuses
             local cwd = function()
@@ -55,7 +77,7 @@ return {
             -- Lualine
             require("lualine").setup({
                 options = {
-                    theme = "auto",
+                    theme = "rose-pine",
                     component_separators = "│",
                     section_separators = { left = "┃", right = "┃" },
                     globalstatus = true,

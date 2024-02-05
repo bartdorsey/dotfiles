890876499a0bbab1e357dfb8a36d346ad3ef5616
Bart Dorsey <bart@bartdorsey.com>
2024-02-05 17:24:36 -0600

    Adding persistence

---
+++ nvim/.config/nvim/lua/plugins/persistence.lua
@@ -0,0 +1,12 @@
+return {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opene
+    opts = {},
+    keys = {
+        {
+            "<leader>rs",
+            "<cmd>lua require('persistence').load()<cr>",
+            { desc = { "Restore Session" } },
+        },
+    },
+}

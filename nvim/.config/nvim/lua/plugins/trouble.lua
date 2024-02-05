ee6af2ae6d72ca66f9c4d07fca1da7b9a3e0aa76
Bart Dorsey <bart@bartdorsey.com>
2024-02-05 17:24:48 -0600

    More binds for Trouble

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,13 +1,46 @@
 return {
-    'folke/trouble.nvim',
-    cmd = {'Trouble', 'TroubleToggle'},
+    "folke/trouble.nvim",
+    cmd = { "Trouble", "TroubleToggle" },
     keys = {
-        { '<leader>xx', '<cmd>TroubleToggle<cr>', desc = "Trouble Toggle"}
+        { "<leader>xx", "<cmd>TroubleToggle<cr>", { desc = "Trouble Toggle" } },
+        {
+            "<leader>xw",
+            "<cmd>TroubleToggle workspace_diagnostics<cr>",
+            { desc = "Trouble Workspace Diagnostics" },
+        },
+        {
+            "<leader>xd",
+            "<cmd>TroubleToggle document_diagnostics<cr>",
+            {
+                desc = "Trouble Document Diagnostics",
+            },
+        },
+        {
+            "<leader>xq",
+            "<cmd>TroubleToggle quickfix<cr>",
+            {
+                desc = "Trouble Quickfix list",
+            },
+        },
+        {
+            "<leader>xl",
+            "<cmd>TroubleToggle loclist<cr>",
+            {
+                desc = "Trouble location list",
+            },
+        },
+        {
+            "gR",
+            "<cmd>Trouble Toggle lsp_references<cr>",
+            {
+                desc = "Trouble LSP References",
+            },
+        },
     },
     dependencies = {
-        'nvim-tree/nvim-web-devicons'
+        "nvim-tree/nvim-web-devicons",
     },
-    config = function ()
-        require('trouble').setup()
-    end
+    config = function()
+        require("trouble").setup()
+    end,
 }

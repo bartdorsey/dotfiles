9419996b9f75b03089f4193de50961659573f12c
Bart Dorsey <bart@bartdorsey.com>
2024-02-05 17:24:57 -0600

    Handy shortcuts to edit neovim config

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -227,3 +227,14 @@ vim.api.nvim_create_autocmd("InsertLeave", {
         vim.opt.relativenumber = true
     end,
 })
+-- sets working dir to neovim config and opens telescope
+vim.api.nvim_create_user_command("Config", function()
+    vim.cmd("cd ~/.dotfiles/nvim/.config/nvim")
+    vim.cmd("edit lua/init.lua")
+end, {})
+
+-- sets working dir to plugins dir and opens Oil
+vim.api.nvim_create_user_command("Plugins", function()
+    vim.cmd("cd ~/.dotfiles/nvim/.config/nvim/lua/plugins")
+    vim.cmd("Oil")
+end, {})

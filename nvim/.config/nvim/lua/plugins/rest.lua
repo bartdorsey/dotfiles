61cca3758d0e371b1a135faf10aeadd1ab7c3088
Bart Dorsey <bart@bartdorsey.com>
2024-02-08 16:31:14 -0600

    Adding bind for rest

--- nvim/.config/nvim/lua/plugins/rest.lua
+++ nvim/.config/nvim/lua/plugins/rest.lua
@@ -1,6 +1,13 @@
 return {
     "rest-nvim/rest.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+        {
+            "<leader>rr",
+            "<Plug>RestNvim<cr>",
+            desc = "Run REST command under cursor",
+        },
+    },
     config = function()
         require("rest-nvim").setup()
     end,

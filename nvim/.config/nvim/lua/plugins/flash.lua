e6eef62b5533655662106bbb753d7d441378bdbd
Bart Dorsey <bart@bartdorsey.com>
2024-02-29 21:17:37 -0600

    Removing S bind

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -16,14 +16,4 @@ return {
             },
         },
     },
-    -- keys = {
-    --     {
-    --         "S",
-    --         mode = { "n", "x", "o" },
-    --         function()
-    --             require("flash").treesitter_search()
-    --         end,
-    --         desc = "Flash Treesitter Select",
-    --     },
-    -- },
 }

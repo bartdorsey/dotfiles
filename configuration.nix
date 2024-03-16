4333a8d7340185ce045bffb565893513ce1fe446
Bart Dorsey <bart@bartdorsey.com>
2024-03-16 15:24:08 -0500

    Trying this for plasma6

--- configuration.nix
+++ configuration.nix
@@ -71,7 +71,7 @@
     enable = true;
   };
 
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   services.xserver.windowManager.awesome = {
     enable = true;

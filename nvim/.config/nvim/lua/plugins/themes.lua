1d5c1cff4e665002a0807e5bb529a6eea38ca858
Bart Dorsey <bart@bartdorsey.com>
2024-03-19 16:46:31 -0500

    Updating rose pine config to not do italics

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -2,6 +2,12 @@
 return {
     "rose-pine/neovim",
     config = function()
+        require("rose-pine").setup({
+            dim_inactive_windows = true,
+            styles = {
+                italic = false,
+            },
+        })
         vim.cmd("colorscheme rose-pine")
     end,
 }

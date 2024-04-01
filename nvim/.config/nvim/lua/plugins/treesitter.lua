0c3132c06a6effcd5fc66a9708a44e96e235f567
Bart Dorsey <bart@bartdorsey.com>
2024-04-01 21:11:44 +0000

    Removing TSUpdate from running on start

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
     dependencies = {
         "windwp/nvim-ts-autotag",
     },
-    build = ":TSUpdate",
     event = { "BufReadPre" },
     config = function()
         -- [[ Configure Treesitter ]]

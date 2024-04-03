a3b8dd279855cbe601e9d42f582917ac02aa0b94
Bart Dorsey <bart@bartdorsey.com>
2024-04-03 18:11:11 +0000

    Removing deprecated org mode setting

--- nvim/.config/nvim/lua/plugins/org-mode.lua
+++ nvim/.config/nvim/lua/plugins/org-mode.lua
@@ -5,9 +5,6 @@ return {
     },
     event = "VeryLazy",
     config = function()
-        -- Load treesitter grammar for org
-        require("orgmode").setup_ts_grammar()
-
         -- Setup treesitter
         require("nvim-treesitter.configs").setup({
             highlight = {

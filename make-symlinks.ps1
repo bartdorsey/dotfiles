1a72bcfcbcf4e6b71d7206ff83d5842d3f384e7b
Bart Dorsey <bart@bartdorsey.com>
2024-04-19 11:55:42 -0500

    Moving user.js into a firefox folder

--- make-symlinks.ps1
+++ make-symlinks.ps1
@@ -9,6 +9,6 @@ New-Link $HOME/.dotfiles/Microsoft.PowerShell_profile.ps1 $PROFILE
 New-Link $HOME/.dotfiles/ssh/.ssh/config $HOME/.ssh/config
 New-Link $HOME/.dotfiles/lazygit/.config/lazygit $HOME/.config/lazygit
 New-Link $HOME/.dotfiles/helix/.config/helix $env:APPDATA/helix
-New-Link $HOME/.dotfiles/user.js $HOME/scoop/persist/firefix/profile/user.js
+New-Link $HOME/.dotfiles/firefox/user.js $HOME/scoop/persist/firefox/profile/user.js
 New-Link $HOME/.dotfiles/wezterm/.config/wezterm/wezterm.lua $HOME/.wezterm.lua
 

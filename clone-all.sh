175894cebda286df9dfa6874eaccd5dd43b0c120
Bart Dorsey <bart@bartdorsey.com>
2024-04-19 11:52:41 -0500

    Removing old clone-all script

--- clone-all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-grm repos sync remote --provider github --access --token-command "pass show github_token" --root ~/github --worktree --force-ssh
-grm repos sync remote --provider gitlab --access --token-command "pass show gitlab_token" --root ~/gitlab --worktree --force-ssh

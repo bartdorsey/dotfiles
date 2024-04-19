3d2da40b25c8ac971cb736ff8b440770ec2208d3
Bart Dorsey <bart@bartdorsey.com>
2024-04-19 11:56:59 -0500

    Updating make-symlinks to not do neovim

--- make-symlinks.ps1
+++ make-symlinks.ps1
@@ -2,8 +2,6 @@ function New-Link ($target, $link) {
     New-Item -Path $link -ItemType SymbolicLink -Value $target -Force
 }
 
-New-Link $HOME/.dotfiles/nvim/.config/nvim $HOME/.config/nvim
-New-Link $HOME/.dotfiles/nvim/.config/nvim $HOME/AppData/Local/nvim
 New-Link $HOME/.dotfiles/starship/.config/starship.toml $HOME/.config/starship.toml
 New-Link $HOME/.dotfiles/Microsoft.PowerShell_profile.ps1 $PROFILE
 New-Link $HOME/.dotfiles/ssh/.ssh/config $HOME/.ssh/config

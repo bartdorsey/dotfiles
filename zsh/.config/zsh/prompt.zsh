014b283005cf60d6fca5f44d6ac2dfb676193968
Bart Dorsey <bart@bartdorsey.com>
2024-06-07 13:30:39 -0500

    Updating oh my posh to use rose pine theme

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -2,5 +2,5 @@ if type starship > /dev/null; then
     # Starship prompt
     eval "$(starship init zsh)"
 elif type oh-my-posh > /dev/null; then
-    eval "$(oh-my-posh init zsh)"
+    eval "$(oh-my-posh init zsh --config $HOME/.config/oh-my-posh/themes/rose-pine.omp.json)"
 fi

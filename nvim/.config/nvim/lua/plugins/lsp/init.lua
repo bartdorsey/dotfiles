f9c2b351ddfad44c2cb786ad3701b2117b5853d5
Bart Dorsey <bart@bartdorsey.com>
2024-09-10 19:47:51 -0500

    Adding ability to add LSP mode to any hosts

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,4 +1,18 @@
-if vim.fn.hostname() == "NZXT" then
+local dev_hosts = {
+    "NZXT",
+    "dev",
+}
+
+function table_contains(tbl, value)
+    for _, v in pairs(tbl) do
+        if v == value then
+            return true
+        end
+    end
+    return false
+end
+
+if table_contains(dev_hosts, vim.fn.hostname()) then
     return {
         "neovim/nvim-lspconfig",
         dependencies = {

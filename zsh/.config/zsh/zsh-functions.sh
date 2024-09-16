69f4e26da64d228ae77e7edd55917f0b12123ba6
Bart Dorsey <bart@bartdorsey.com>
2024-09-16 18:38:20 -0500

    adding aurinstall helper function

--- zsh/.config/zsh/zsh-functions.sh
+++ zsh/.config/zsh/zsh-functions.sh
@@ -17,6 +17,16 @@ function _activate_venv() {
     fi
 }
 
+aurinstall() {
+    local AURL="https://aur.archlinux.org/cgit/aur.git/snapshot/${1}.tar.gz"
+    local WDIR="$(mktemp -d)"
+    (cd "${WDIR}" && curl -Ofs "${AURL}" && tar -xzf "${1}.tar.gz" || {
+        echo 'Package not found' 1>&2
+        false
+    }) && (cd "${WDIR}/${1}" && makepkg -sic)
+    rm -rf "${WDIR}"
+}
+
 typeset -ag precmd_functions;
  if [[ -z "${precmd_functions[(r)_activate_venv]+1}" ]]; then
      precmd_functions=( _activate_venv ${precmd_functions[@]} )

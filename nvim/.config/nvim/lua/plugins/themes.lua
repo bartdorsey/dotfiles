17361e86141df2d2a13cc60630dd79c4f8408fd4
Bart Dorsey <bart@bartdorsey.com>
2024-09-20 12:05:06 -0500

    Switching to better rose pine theme

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -1,9 +1,10 @@
 -- Theme
 return {
-    "tinted-theming/base16-vim",
+    "rose-pine/neovim",
     config = function()
-        vim.g.base16_colorspace = 256
-        vim.g.base16_background_transparent = 1
-        vim.cmd("colorscheme base16-rose-pine")
+        require("rose-pine").setup({
+            dim_inactive_windows = true,
+        })
+        vim.cmd("colorscheme rose-pine")
     end,
 }

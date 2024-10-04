9f45f1da2632c02f601b1a72d8d79bfe820123e2
Bart Dorsey <bart@bartdorsey.com>
2024-10-04 17:31:27 -0500

    Removing todo comments

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'folke/todo-comments.nvim',
-    dependencies = { "nvim-lua/plenary.nvim"},
-    cmd = {"TodoTrouble", "TodoTelescope"},
-    config = function()
-        require('todo-comments').setup()
-    end
-}

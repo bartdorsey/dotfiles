9666be53b82b1914eb835b46a814559039d57ae0
Bart Dorsey <bart@bartdorsey.com>
2024-10-11 16:44:47 -0500

    update make-symlinks.ps1 to support vscode

--- make-symlinks.ps1
+++ make-symlinks.ps1
@@ -1,5 +1,6 @@
-function New-Link ($target, $link) {
-    New-Item -Path $link -ItemType SymbolicLink -Value $target -Force
+function New-Link ($target, $link)
+{
+	New-Item -Path $link -ItemType SymbolicLink -Value $target -Force
 }
 
 New-Link $HOME/.dotfiles/starship/.config/starship.toml $HOME/.config/starship.toml
@@ -10,3 +11,6 @@ New-Link $HOME/.dotfiles/helix/.config/helix $env:APPDATA/helix
 New-Link $HOME/.dotfiles/firefox/user.js $HOME/scoop/persist/firefox/profile/user.js
 New-Link $HOME/.dotfiles/wezterm/.config/wezterm/wezterm.lua $HOME/.wezterm.lua
 New-Link $HOME/.dotfiles/oh-my-posh/.config/oh-my-posh $HOME/.config/oh-my-posh
+New-Link $HOME/.dotfiles/vscode/.config/Code/User/settings.json $env:APPDATA/Code/User/settings.json
+New-Link $HOME/.dotfiles/vscode/.config/Code/User/snippets $env:APPDATA/Code/User/snippets
+New-Link $HOME/.dotfiles/vscode/.config/Code/User/tasks.json $env:APPDATA/Code/User/tasks.json

529da7ca91558a9bc03af08aa9d65600d8f75d69
Bart Dorsey <bart@bartdorsey.com>
2024-10-23 10:43:48 -0500

    Moving nix and windows files into folder

--- windows/make-symlinks.ps1
+++ windows/make-symlinks.ps1
@@ -4,7 +4,7 @@ function New-Link ($target, $link)
 }
 
 New-Link $HOME/.dotfiles/starship/.config/starship.toml $HOME/.config/starship.toml
-New-Link $HOME/.dotfiles/Microsoft.PowerShell_profile.ps1 $PROFILE
+New-Link $HOME/.dotfiles/windows/Microsoft.PowerShell_profile.ps1 $PROFILE
 New-Link $HOME/.dotfiles/ssh/.ssh/config $HOME/.ssh/config
 New-Link $HOME/.dotfiles/lazygit/.config/lazygit $HOME/.config/lazygit
 New-Link $HOME/.dotfiles/helix/.config/helix $env:APPDATA/helix

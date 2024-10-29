00d364de33866e295c6b3dcd31b6475f11dfea06
Bart Dorsey <bart@bartdorsey.com>
2024-10-29 11:55:28 -0500

    Updating nvimlint to include cspell. Updating lazy lock

--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -9,19 +9,28 @@ if os.getenv("DEVMODE") then
 
             if which("ruff") then
                 table.insert(python_linters, "ruff")
+                vim.notify("ruff enabled.")
             end
 
             if which("mypy") then
                 table.insert(python_linters, "mypy")
+                vim.notify("mypy enabled.")
             end
 
             if which("flake8") then
                 table.insert(python_linters, "flake8")
+                vim.notify("flake8 enabled.")
             end
 
-            --            if which("markdownlint") or which("markdownlint-cli") then
-            table.insert(markdown_linters, "markdownlint")
-            --end
+            if which("cspell") then
+                table.insert(markdown_linters, "cspell")
+                vim.notify("cspell enabled.")
+            end
+
+            if which("markdownlint") or which("markdownlint-cli") then
+                table.insert(markdown_linters, "markdownlint")
+                vim.notify("markdownlint enabled.")
+            end
 
             -- print out the linters
             require("lint").linters_by_ft = {

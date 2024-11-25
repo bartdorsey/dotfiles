a7bc7eed78673b13d129399bc2335462479f8268
Bart Dorsey <bart@bartdorsey.com>
2024-11-25 12:48:52 -0600

    Adding transparency to backgrounds

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -8,5 +8,37 @@ return {
             dim_inactive_windows = true,
         })
         vim.cmd("colorscheme rose-pine")
+
+        -- Make background transparent
+        local groups = {
+            "Normal",
+            "NormalNC",
+            "Comment",
+            "Constant",
+            "Special",
+            "Identifier",
+            "Statement",
+            "PreProc",
+            "Type",
+            "Underlined",
+            "Todo",
+            "String",
+            "Function",
+            "Conditional",
+            "Repeat",
+            "Operator",
+            "Structure",
+            "LineNr",
+            "NonText",
+            "SignColumn",
+            "CursorLine",
+            "CursorLineNr",
+            "StatusLine",
+            "StatusLineNC",
+            "EndOfBuffer",
+        }
+        for _, v in ipairs(groups) do
+            vim.api.nvim_set_hl(0, v, { bg = "NONE", ctermbg = "NONE" })
+        end
     end,
 }

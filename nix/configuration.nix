0e2320b5b465846e7b5ea4347afa7c7781262c89
Bart Dorsey <bart@bartdorsey.com>
2024-11-29 13:32:49 -0600

    Updating xrandr

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -69,7 +69,7 @@
   services.xserver.enable = true;
 
   services.xserver = {
-    displayManager.setupCommands = "${pkgs.xorg.xrandr}/bin/xrandr --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate left --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 1440x1952 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 1440x512 --rotate normal --output DP-5 --off";
+    displayManager.setupCommands = "${pkgs.xorg.xrandr}/bin/xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rotate left --output HDMI-0 --mode 1920x1080 --pos 1440x1952 --rotate normal --output DP-4 --primary --mode 2560x1440 --pos 1440x512 --rotate normal";
 
     desktopManager = {
       xterm.enable = false;

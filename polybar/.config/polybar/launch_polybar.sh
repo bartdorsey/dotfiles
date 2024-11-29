e7746a3be7587924814a24a5df7609220be7b90b
Bart Dorsey <bart@bartdorsey.com>
2024-11-29 13:32:55 -0600

    Updating polybar

--- polybar/.config/polybar/launch_polybar.sh
+++ polybar/.config/polybar/launch_polybar.sh
@@ -1,3 +1,16 @@
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-  MONITOR=$m polybar --reload $m &
+#!/usr/bin/env bash
+
+# Get the primary monitor
+PRIMARY_MONITOR=$(xrandr --query | grep " connected primary" | cut -d" " -f1)
+
+# Launch the primary bar on the primary monitor
+if [ -n "$PRIMARY_MONITOR" ]; then
+    MONITOR=$PRIMARY_MONITOR polybar --reload primary &
+fi
+
+# Launch the secondary bar on all other monitors
+for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload secondary &
 done
+
+

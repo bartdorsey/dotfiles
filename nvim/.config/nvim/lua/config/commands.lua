e04b8ae8c1ca3a59ef92afa67e101568ae71e4bb
Bart Dorsey <bart@bartdorsey.com>
2024-12-05 13:52:04 -0600

    Removing alpha loader function

--- nvim/.config/nvim/lua/config/commands.lua
+++ nvim/.config/nvim/lua/config/commands.lua
@@ -1,18 +1,3 @@
--- When closing the last window, load alpha back up
-vim.api.nvim_create_user_command("Q", function()
-    local multiple_windows, _ =
-        pcall(vim.api.nvim_win_close, vim.fn.win_getid(), false)
-
-    if multiple_windows then
-        vim.cmd.bd()
-    elseif vim.o.ft == "alpha" then
-        vim.cmd.quitall()
-    else
-        vim.cmd.bd()
-        vim.cmd.Alpha()
-    end
-end, {})
-
 -- sets working dir to neovim config and opens telescope
 vim.api.nvim_create_user_command("Config", function()
     vim.cmd("cd ~/.dotfiles/nvim/.config/nvim")

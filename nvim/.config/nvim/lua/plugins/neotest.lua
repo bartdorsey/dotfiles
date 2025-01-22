af663b05d3368810e4ad993c49080731d6760cae
Bart Dorsey <bart@bartdorsey.com>
2025-01-22 16:10:24 -0600

    Adding neotest

---
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,17 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-neotest/neotest-python",
+    },
+    config = function()
+        require("neotest").setup({
+            adapters = {
+                require("neotest-python"),
+            },
+        })
+    end,
+}

9f4536204dfcfe8878c2f2f7696630ab0144e852
Bart Dorsey <bart@bartdorsey.com>
2025-02-06 11:04:07 -0600

    changes from nzxt-wsl on Thu Feb 6 11:04:07 AM CST 2025

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -40,7 +40,9 @@ return {
         -- see below for full list of optional dependencies 👇
     },
     opts = {
-        ui.enable = false,
+        ui = {
+            enable = false,
+        },
         workspaces = {
             {
                 name = "personal",

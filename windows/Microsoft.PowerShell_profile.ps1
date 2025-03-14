d52030e9b83a500f17776b90c5691048d057cd3e
Bart Dorsey <bart@bartdorsey.com>
2025-03-13 21:43:35 -0500

    updating windows profile

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,27 +1,9 @@
+Import-Module "Microsoft.WinGet.CommandNotFound"
+Import-Module "scoop-completion"
+
 function Get-Disk-Free
 {
 	Get-PSDrive -PSProvider FileSystem
-}
-
-function Build-Django-Project
-{
-	python -m venv .venv
-	.\.venv\Scripts\Activate.ps1
-	pip install -r requirements.txt
-	python manage.py migrate
-	python manage.py createsuperuser
-}
-
-
-function Build-Python-Virtual-Environment
-{
-	python -m venv .venv
-	.\.venv\Scripts\Activate.ps1
-}
-
-function Get-Projects
-{
-	fd -t d -d 1 . $env:USERPROFILE\dev\bartdorsey $env:USERPROFILE\dev\galvanize $env:USERPROFILE\projects | fzf | % { code $_ }
 }
 
 # Set PS to Emacs mode
@@ -36,55 +18,27 @@ $env:HOME = $env:USERPROFILE
 $env:DEVMODE = 1
 $env:VIMINIT = "source ~/.config/vim/vimrc"
 # Aliases
-Set-Alias p Get-Projects
-Set-Alias open explorer.exe
-Set-Alias w whoami
-Set-Alias df Get-Disk-Free
-Set-Alias venv Build-Python-Virtual-Environment
-Set-Alias which Get-Command
-Set-Alias vim nvim
-Set-Alias dsetup Build-Django-Project-Setup
-Set-Alias lg lazygit
-Set-Alias ls lsd
-#Set-Alias docker podman
 
-function Invoke-Exa
-{
-	eza --icons --git -F -h -G --group-directories-first $args
-}
-# Set-Alias ls Invoke-Exa
-
-Import-Module Terminal-Icons
+@{
+    p      = 'Get-Projects'
+    open   = 'explorer.exe'
+    w      = 'whoami'
+    df     = 'Get-Disk-Free'
+    venv   = 'Build-Python-Virtual-Environment'
+    which  = 'Get-Command'
+    vim    = 'nvim'
+    dsetup = 'Build-Django-Project-Setup'
+    lg     = 'lazygit'
+    ls     = 'lsd'
+}.GetEnumerator() | ForEach-Object { Set-Alias $_.Key $_.Value }
 
 fnm env --use-on-cd --shell power-shell | Out-String | Invoke-Expression
 
 Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
-
-
 Invoke-Expression (&starship init powershell)
-#oh-my-posh init pwsh --config $env:USERPROFILE/.config/oh-my-posh/themes/rose-pine.omp.json | Invoke-Expression
-Import-Module scoop-completion
 Invoke-Expression (&scoop-search --hook)
 
-# Dotfiles
-$DotFilesPath = Join-Path $HOME '.dotfiles'
 
-$DotFilesAutodetect = $true
-
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile))
-{
-	Import-Module "$ChocolateyProfile"
+if ($Host.Name -eq 'ConsoleHost') {
+	fastfetch --disk-show-external false
 }
-
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-
-Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
-
-fastfetch --disk-show-external false

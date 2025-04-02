72337740b0165223970afd1e00b034417e19a378
Bart Dorsey <bart@bartdorsey.com>
2025-04-02 16:15:41 -0500

    Updating helpview

--- nvim/.config/nvim/lua/plugins/helpview.lua
+++ nvim/.config/nvim/lua/plugins/helpview.lua
@@ -4,4 +4,25 @@ return {
         "catppuccin/nvim",
     },
     lazy = false,
+    opts = {
+        preview = {
+            icon_provider = "devicons",
+        },
+        vimdoc = {
+            headings = {
+                heading_1 = {
+                    sign = "  ",
+                },
+                heading_2 = {
+                    sign = " 󰉬 ",
+                },
+                heading_3 = {
+                    sign = " 󰉭 ",
+                },
+                heading_4 = {
+                    sign = " 󰉮 ",
+                },
+            },
+        },
+    },
 }

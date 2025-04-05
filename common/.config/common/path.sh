e7495cdac99a989d42b0dafaa0bfb7d92f65d210
Bart Dorsey <bart@bartdorsey.com>
2025-04-04 19:39:16 -0500

    Adding flatpak support

--- common/.config/common/path.sh
+++ common/.config/common/path.sh
@@ -84,3 +84,8 @@ if [ -d "$HOME/.config/emacs/bin" ]; then
     export PATH="$HOME/.config/emacs/bin:$PATH"
 fi
 
+# Flatpak
+if command_exists flatpak; then
+    export XDG_DATA_DIRS=$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share
+fi
+

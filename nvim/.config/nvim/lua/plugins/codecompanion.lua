b772e30eef50b9a274b8c3d7f0a0ad5fbf9700d2
Bart Dorsey <bart@bartdorsey.com>
2025-04-04 19:41:20 -0500

    Updating neovim plugins

---
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,50 @@
+return {
+    "olimorris/codecompanion.nvim",
+    opts = {
+        strategies = {
+            chat = {
+                adapter = "copilot",
+            },
+            inline = {
+                adapter = "copilot",
+            },
+            cmd = {
+                adapter = "copilot",
+            },
+        },
+        adapters = {
+            copilot = function()
+                return require("codecompanion.adapters").extend("copilot", {
+                    schema = {
+                        model = {
+                            default = "claude-3.7-sonnet",
+                        },
+                    },
+                })
+            end,
+            openai = function()
+                return require("codecompanion.adapters").extend("openai", {
+                    env = {
+                        api_key = "cmd:pass openai",
+                    },
+                })
+            end,
+            ollama = function()
+                return require("codecompanion.adapters").extend("ollama", {
+                    env = {
+                        url = "http://10.0.0.72:11434",
+                    },
+                    schema = {
+                        num_ctx = {
+                            default = 20000,
+                        },
+                    },
+                })
+            end,
+        },
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+}
--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,5 +1,4 @@
 return {
-    -- "github/copilot.vim",
     {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",

cc0232e45b13139b22f17d0f57808c7667ac00e4
Bart Dorsey <bart@bartdorsey.com>
2025-05-03 19:32:37 -0500

    copySystemConfiguration not supported with flakes

--- nix/hosts/media/configuration.nix
+++ nix/hosts/media/configuration.nix
@@ -25,8 +25,6 @@
 
   system.stateVersion = "24.11"; # Did you read the comment?
 
-  system.copySystemConfiguration = true;
-
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = false;
 }

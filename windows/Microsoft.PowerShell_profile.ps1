06a7050d242eafcf09f2e0dd83c31294a9ee97f9
Bart Dorsey <bart@bartdorsey.com>
2025-06-03 20:27:03 -0500

    Changing windows profile

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -13,10 +13,10 @@ Set-PSReadlineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Key 'Ctrl+d' -Function DeleteCharOrExit
 
 $env:EDITOR = "code"
-#$env:XDG_CONFIG_HOME = "$env:USERPROFILE\.config"
-#$env:HOME = $env:USERPROFILE
+$env:XDG_CONFIG_HOME = "$env:USERPROFILE\.config"
+$env:HOME = $env:USERPROFILE
 $env:DEVMODE = 1
-#$env:VIMINIT = "source ~/.config/vim/vimrc"
+$env:VIMINIT = "source ~/.config/vim/vimrc"
 # Aliases
 
 @{

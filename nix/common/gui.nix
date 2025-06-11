29693400ab6ed569132c8134c6d0f4fe978fd86f
Bart Dorsey <bart@bartdorsey.com>
2025-06-11 11:15:52 -0500

    Fixing pulse audio

--- nix/common/gui.nix
+++ nix/common/gui.nix
@@ -59,7 +59,7 @@
   services.xserver.upscaleDefaultCursor = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;

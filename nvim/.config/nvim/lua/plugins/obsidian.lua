aae3ec61c8db648ea4d47b3c86692f4f352350f7
Bart Dorsey <bart@bartdorsey.com>
2025-07-31 11:09:52 -0500

    Condtionally load obsidian plugin

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -2,6 +2,9 @@ return {
     "obsidian-nvim/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
     lazy = true,
+    cond = function()
+        return vim.fn.isdirectory(vim.fn.expand("~/Obsidian")) == 1
+    end,
     keys = {
         {
             "<leader>os",

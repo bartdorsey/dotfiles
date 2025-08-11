d49da5ab26b4d43fabbd14df543db6c8a4404f5d
Bart Dorsey <bart@bartdorsey.com>
2025-08-11 15:42:48 -0500

    Switching to longer version of fd command

--- common/.config/common/env.sh
+++ common/.config/common/env.sh
@@ -64,7 +64,7 @@ export PASSWORD_STORE_DIR="${XDG_DATA_HOME}/pass"
 
 # FZF Config
 if type fzf > /dev/null 2>&1; then
-    export FZF_DEFAULT_COMMAND='fd --type f --color=never --hidden'
+    export FZF_DEFAULT_COMMAND='fdfind --type f --color=never --hidden'
     export FZF_DEFAULT_OPTS='--no-height'
 fi
 

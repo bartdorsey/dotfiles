00b53e3d406c105a9efa7d79bc211e0070f7ed45
Bart Dorsey <bart@bartdorsey.com>
2025-08-17 12:15:19 -0500

    Adding firefox prefs

--- firefox/user.js
+++ firefox/user.js
@@ -326,3 +326,7 @@ user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
 user_pref("general.smoothScroll.currentVelocityWeighting", 1.0);
 user_pref("general.smoothScroll.stopDecelerationWeighting", 1.0);
 user_pref("mousewheel.default.delta_multiplier_y", 300); // 250-400; adjust this number to your liking
+
+// My prefs
+user_pref("browser.warnOnQuitShortcut", false);
+user_pref("browser.tabs.firefox-view", false);

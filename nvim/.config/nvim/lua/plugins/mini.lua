d48cb18ebb4eec52d0979abb2f4a7957e551cb94
Bart Dorsey <bart@bartdorsey.com>
2025-08-21 22:17:19 -0500

    Removing unused mini plugins

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -12,23 +12,6 @@ return {
         require("mini.ai").setup({})
         -- Pairs
         require("mini.pairs").setup({})
-        -- Files
-        require("mini.files").setup({
-            options = {
-                use_as_default_explorer = true,
-            },
-        })
-        -- Files keymap
-        vim.keymap.set("n", "<leader>-", ":lua MiniFiles.open()<cr>", {
-            desc = "Open Mini Files",
-        })
-
-        -- Sessions
-        local MiniSessions = require("mini.sessions")
-        MiniSessions.setup({
-            directory = vim.fn.stdpath("data") .. "/sessions",
-        })
-
         -- Starter
         local starter = require("mini.starter")
         starter.setup({

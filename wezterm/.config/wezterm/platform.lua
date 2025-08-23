dbc975650c4996843df2a42602a74102da3bb07d
Bart Dorsey <bart@bartdorsey.com>
2025-08-22 21:54:24 -0500

    changing windows background

--- wezterm/.config/wezterm/platform.lua
+++ wezterm/.config/wezterm/platform.lua
@@ -8,7 +8,7 @@ function M.apply_platform_config(config)
         config.front_end = "OpenGL"
         config.font = wezterm.font("AtkynsonMono NF", { weight = "DemiBold" })
         config.font_size = 14
-        config.win32_system_backdrop = "Mica"
+        config.win32_system_backdrop = "Auto"
         config.window_background_opacity = 0.9
     elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
         config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"

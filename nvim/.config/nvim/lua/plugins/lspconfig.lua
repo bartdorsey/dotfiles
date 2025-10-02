a4101fc7f87e1c08df319d94333703f57686e07e
Bart Dorsey <bart@bartdorsey.com>
2025-10-01 20:40:10 -0500

    Switching back to based pyright

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -128,7 +128,7 @@ return {
                     },
                 },
             },
-            pyright = {
+            basedpyright = {
                 settings = {
                     python = {
                         analysis = {

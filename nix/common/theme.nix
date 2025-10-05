1a1c88d5c24cd82aac8a13c9191bb1ae6c4cdd4d
Bart Dorsey <bart@bartdorsey.com>
2025-10-05 15:21:40 -0500

    Updating btop

---
+++ nix/common/theme.nix
@@ -0,0 +1,20 @@
+{...}: {
+  console.colors = [
+    "282828" # black (bg)
+    "cc241d" # red
+    "98971a" # green
+    "d79921" # yellow
+    "458588" # blue
+    "b16286" # magenta
+    "689d6a" # cyan
+    "a89984" # white (fg)
+    "928374" # bright black (gray)
+    "fb4934" # bright red
+    "b8bb26" # bright green
+    "fabd2f" # bright yellow
+    "83a598" # bright blue
+    "d3869b" # bright magenta
+    "8ec07c" # bright cyan
+    "ebdbb2" # bright white
+  ];
+}

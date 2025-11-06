40b33c1be9d4103109e3c374c4ef405928fda907
Bart Dorsey <bart@bartdorsey.com>
2025-11-06 16:37:07 -0600

    Updating to Acrylic

--- wezterm/.config/wezterm/platform.lua
+++ wezterm/.config/wezterm/platform.lua
@@ -8,7 +8,7 @@ function M.apply_platform_config(config)
         config.front_end = "OpenGL"
         config.font = wezterm.font("Iosevka Nerd Font", { weight = "DemiBold" })
         config.font_size = 14
-        config.win32_system_backdrop = "Auto"
+        config.win32_system_backdrop = "Acrylic"
         config.window_background_opacity = 0.9
     elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
         -- config.window_decorations = "RESIZE"

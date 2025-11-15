e2b602bf90529664555bcb1c30d389b448c698dc
Bart Dorsey <bart@bartdorsey.com>
2025-11-14 20:02:04 -0600

    Adding firewall back and allowing vpn

--- nix/hosts/nzxt/configuration.nix
+++ nix/hosts/nzxt/configuration.nix
@@ -53,7 +53,11 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall = {
+    enable = true;
+    allowedUDPPorts = [51820];
+    checkReversePath = false;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions

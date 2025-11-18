ec0e89cc6215dd0dc99ac7696dcdd793c6802de0
Bart Dorsey <bart@bartdorsey.com>
2025-11-18 12:44:42 -0600

    Updating gnupg stuff

--- zsh/.config/zsh/wsl.zsh
+++ zsh/.config/zsh/wsl.zsh
@@ -1,21 +1,4 @@
 if [[ -v WSL_DISTRO_NAME ]]; then
-    # Configure ssh forwarding
-    #export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-    # need `ps -ww` to get non-truncated command for matching
-    # use square brackets to generate a regex match for the process we want but that doesn't match the grep command running it!
-    #ALREADY_RUNNING=$(ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $?)
-    #if [[ $ALREADY_RUNNING != "0" ]]; then
-    #   if [[ -S $SSH_AUTH_SOCK ]]; then
-    #      # not expecting the socket to exist as the forwarding command isn't running (http://www.tldp.org/LDP/abs/html/fto.html)
-    #      echo "removing previous socket..."
-    #      rm $SSH_AUTH_SOCK
-    #   fi
-    #   echo "Starting SSH-Agent relay..."
-    #   # setsid to force new session to keep running
-    #   # set socat to listen on $SSH_AUTH_SOCK and forward to npiperelay which then forwards to openssh-ssh-agent on windows
-    #   (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
-    #fi
-
     # export WSL_INTEROP=
     # for socket in /run/WSL/*; do
     #     if ss -elx | grep -q "$socket"; then
@@ -48,7 +31,7 @@ if [[ -v WSL_DISTRO_NAME ]]; then
 
     notify-send() { wsl-notify-send.exe --category $WSL_DISTRO_NAME "${@}"; }
 
-    export PATH="/mnt/c/Windows:/mnt/c/Windows/System32:/mnt/c/Users/bart/AppData/Local/Programs/Microsoft VS Code/bin:/mnt/c/Users/bart/scoop/apps/neovide/current:/mnt/c/Program Files/PowerShell/7:$PATH"
+    export PATH="/mnt/c/Windows:/mnt/c/Windows/System32:/mnt/c/Users/bart/AppData/Local/Programs/Microsoft VS Code/bin:/mnt/c/Program Files/PowerShell/7:/mnt/c/Users/bart/scoop/apps/gpg4win/current/GnuPG/bin:/mnt/c/Windows/System32/WindowsPowerShell/v1.0:$PATH"
 
     export DISPLAY=:0
 fi

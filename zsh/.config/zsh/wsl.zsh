19d1dbb59fe72e4a331a459c1f300108bc17846d
Bart Dorsey <bart@bartdorsey.com>
2025-11-21 20:29:47 -0600

    Adding zed to path in WSL

--- zsh/.config/zsh/wsl.zsh
+++ zsh/.config/zsh/wsl.zsh
@@ -1,4 +1,19 @@
 if [[ -v WSL_DISTRO_NAME ]]; then
+    # export GPG_AGENT_SOCK="/run/user/1000/gnupg/d.9yfkc3bkj8nfdmckpq61pgou/S.gpg-agent"
+    # ss -a | grep -q "$GPG_AGENT_SOCK"
+    # if ! ss -a | grep -q "$GPG_AGENT_SOCK"; then
+      # rm -rfv "$GPG_AGENT_SOCK"
+      # wsl2_ssh_pageant_bin="/mnt/c/Users/bart/Apps/wsl2-ssh-pageant.exe"
+      # if test -x "$wsl2_ssh_pageant_bin"; then
+      #     echo "Starting socat"
+      #   /usr/bin/socat UNIX-LISTEN:"$GPG_AGENT_SOCK,fork" EXEC:"$wsl2_ssh_pageant_bin -gpg S.gpg-agent -gpgConfigBasepath C\:/Users/bart/AppData/Local/gnupg" &
+      # else
+      #   echo >&2 "WARNING: $wsl2_ssh_pageant_bin is not executable."
+      # fi
+      # unset wsl2_ssh_pageant_bin
+    # else
+    #     echo "No $GPG_AGENT_SOCK"
+    # fi
     # export WSL_INTEROP=
     # for socket in /run/WSL/*; do
     #     if ss -elx | grep -q "$socket"; then
@@ -31,7 +46,7 @@ if [[ -v WSL_DISTRO_NAME ]]; then
 
     notify-send() { wsl-notify-send.exe --category $WSL_DISTRO_NAME "${@}"; }
 
-    export PATH="/mnt/c/Windows:/mnt/c/Windows/System32:/mnt/c/Users/bart/AppData/Local/Programs/Microsoft VS Code/bin:/mnt/c/Program Files/PowerShell/7:/mnt/c/Users/bart/scoop/apps/gpg4win/current/GnuPG/bin:/mnt/c/Windows/System32/WindowsPowerShell/v1.0:$PATH"
+    export PATH="/mnt/c/Windows:/mnt/c/Windows/System32:/mnt/c/Users/bart/AppData/Local/Programs/Microsoft VS Code/bin:/mnt/c/Program Files/PowerShell/7:/mnt/c/Users/bart/scoop/apps/gpg4win/current/GnuPG/bin:/mnt/c/Windows/System32/WindowsPowerShell/v1.0:/mnt/c/Users/bart/AppData/Local/Programs/Zed/bin:$PATH"
     export BROWSER="pwsh.exe -noni -nop -c start"
 
     export DISPLAY=:0

84920d2a61061621eb662c54d671844b8fa86c94
Bart Dorsey <bart@bartdorsey.com>
2025-12-15 13:01:36 -0600

    yasb

--- glzr/.glzr/glazewm/reload.ps1
+++ glzr/.glzr/glazewm/reload.ps1
@@ -1,7 +1,7 @@
 taskkill /IM zebar.exe /F
-taskkill /IM tacky-borders.exe /F
+#taskkill /IM tacky-borders.exe /F
 # taskkill /IM glaze-autotiler.exe /F
 Start-Process zebar -NoNewWindow
-Start-Process C:\Users\bart\Apps\tacky-borders.exe -NoNewWindow
+#Start-Process C:\Users\bart\Apps\tacky-borders.exe -NoNewWindow
 # Start-Process C:\Users\bart\Apps\glaze-autotiler.exe -NoNewWindow
 exit
--- glzr/.glzr/glazewm/startup.ps1
+++ glzr/.glzr/glazewm/startup.ps1
@@ -1,4 +1,4 @@
 Start-Process zebar -NoNewWindow
-Start-Process C:\Users\bart\Apps\tacky-borders.exe -NoNewWindow
+#Start-Process C:\Users\bart\Apps\tacky-borders.exe -NoNewWindow
 # Start-Process C:\Users\bart\Apps\glaze-autotiler.exe -NoNewWindow
 exit

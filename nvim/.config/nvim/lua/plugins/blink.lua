c41dd89e60b3aa8667b94f6a6fbeee0385748caa
Bart Dorsey <bart@bartdorsey.com>
2025-12-24 12:50:23 -0600

    Switching to default completion. (Rememebr control-y dude)

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -32,7 +32,7 @@ return {
         -- C-k: Toggle signature help (if signature.enabled = true)
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = "super-tab" },
+        keymap = { preset = "default" },
         cmdline = {
             completion = {
                 menu = {

09b203404bb522d77eb771f47b5bbadb97f2a2ea
Bart Dorsey <bart@bartdorsey.com>
2026-01-16 10:41:24 -0600

    Fixing greeter and also hyprland config

--- nix/common/display-manager.nix
+++ nix/common/display-manager.nix
@@ -1,27 +1,25 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   # Enable Display Manager
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet \
+        command = "${pkgs.tuigreet}/bin/tuigreet \
           --time --time-format '%I:%M %p | %a • %h | %F' \
           --cmd 'uwsm start hyprland'";
-        user    = "greeter";
+        user = "greeter";
       };
     };
   };
 
   users.users.greeter = {
     isNormalUser = false;
-    description  = "greetd greeter user";
-    extraGroups  = [ "video" "audio" ];
-    linger        = true;
+    description = "greetd greeter user";
+    extraGroups = ["video" "audio"];
+    linger = true;
   };
 
   environment.systemPackages = with pkgs; [
-    greetd.tuigreet
+    tuigreet
   ];
 }

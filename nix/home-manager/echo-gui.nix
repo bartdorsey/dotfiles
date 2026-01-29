aa5e4d7ad0b3e182f60612c367914da2a55eb328
Bart Dorsey <bart@bartdorsey.com>
2026-01-28 20:11:40 -0600

    Moving hyprland stuff to a separate nix file

--- nix/home-manager/echo-gui.nix
+++ nix/home-manager/echo-gui.nix
@@ -13,34 +13,6 @@
     enable = true;
   };
 
-  services.hyprpaper.enable = true;
-  services.hypridle = {
-    enable = true;
-    settings = {
-      general = {
-        lock_cmd = "hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
-      };
-      listener = [
-        {
-          timeout = 150;
-          on-timeout = "brightnessctl -s set 10";
-          on-resume = "brightnessctl -r";
-        }
-        {
-          timeout = 300;
-          on-timeout = "loginctl lock-session";
-        }
-        {
-          timeout = 330;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
-        }
-      ];
-    };
-  };
-
   systemd.user.services.onepassword = {
     Unit = {
       Description = "1Password";
@@ -81,19 +53,6 @@
       name = "Capitaine Cursors (Gruvbox)";
       package = pkgs.capitaine-cursors-themed;
     };
-  };
-
-  home.pointerCursor = {
-    enable = true;
-    x11 = {
-      enable = true;
-    };
-    gtk.enable = true;
-    hyprcursor = {
-      enable = true;
-    };
-    name = "Capitaine Cursors (Gruvbox)";
-    package = pkgs.capitaine-cursors-themed;
   };
 
   home.file.".config/xdg-settings/default-web-browser".text = "zen.desktop";

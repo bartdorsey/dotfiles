b372ce302a0cf2da696df4bb823ff892d84b828c
Bart Dorsey <bart@bartdorsey.com>
2026-01-31 00:36:32 -0600

    Cosmic updates

---
+++ nix/common/virtual-machines.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+    };
+  };
+
+  users.users.echo = {
+    extraGroups = ["libvirtd"];
+  };
+}

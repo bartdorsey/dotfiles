208831290bae5375cd2e39874cb4235d38f93bf5
Bart Dorsey <bart@bartdorsey.com>
2026-01-30 22:37:44 -0600

    Adding some cosmic settings

--- nix/common/cosmic.nix
+++ nix/common/cosmic.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   # Enable the COSMIC login manager
   services.displayManager.cosmic-greeter.enable = true;
 
@@ -6,6 +6,17 @@
   services.desktopManager.cosmic.enable = true;
 
   services.system76-scheduler.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    cosmic-ext-applet-external-monitor-brightness
+    cosmic-ext-applet-caffeine
+    cosmic-ext-applet-minimon
+    cosmic-ext-applet-privacy-indicator
+    cosmic-ext-calculator
+    cosmic-ext-ctl
+    cosmic-ext-tweaks
+    quick-webapps
+  ];
 
   environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
 

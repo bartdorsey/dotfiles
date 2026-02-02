6ff2340c60306c8b04d01fdda5d6ea11b337ea0e
Bart Dorsey <bart@bartdorsey.com>
2026-02-02 17:03:50 -0600

    Updating domain

--- wezterm/.config/wezterm/domains.lua
+++ wezterm/.config/wezterm/domains.lua
@@ -17,6 +17,41 @@ function M.get_ssh_hosts()
     return filtered_hosts
 end
 
+local function distro_icon(name)
+    wezterm.log_info(name)
+    if string.find(name, "Debian") then
+        return wezterm.format({
+            { Foreground = { AnsiColor = "Red" } },
+            { Text = wezterm.nerdfonts.linux_debian },
+        })
+    elseif string.find(name, "Ubuntu") then
+        return wezterm.format({
+            { Foreground = { Color = "orange" } },
+            { Text = wezterm.nerdfonts.linux_ubuntu },
+        })
+    elseif string.find(name, "NixOS") then
+        return wezterm.format({
+            { Foreground = { Color = "slateblue" } },
+            { Text = wezterm.nerdfonts.linux_nixos },
+        })
+    elseif string.find(name, "Arch") then
+        return wezterm.format({
+            { Foreground = { Color = "lightblue" } },
+            { Text = wezterm.nerdfonts.linux_archlinux },
+        })
+    elseif string.find(name, "Fedora") then
+        return wezterm.format({
+            { Foreground = { Color = "lightblue" } },
+            { Text = wezterm.nerdfonts.linux_fedora },
+        })
+    else
+        return wezterm.format({
+            { Foreground = { Color = "yellow" } },
+            { Text = wezterm.nerdfonts.linux_tux },
+        })
+    end
+end
+
 function M.get_launch_menu(pane)
     local entries = {}
 
@@ -28,30 +63,30 @@ function M.get_launch_menu(pane)
                 cwd = nil
             end
             table.insert(entries, {
-                label = wezterm.nerdfonts.linux_tux .. " " .. wsl_domain.name,
+                label = distro_icon(wsl_domain.name) .. "  " .. wsl_domain.name,
                 args = {},
                 domain = { DomainName = wsl_domain.name },
                 cwd = cwd,
             })
         end
         table.insert(entries, {
-            label = wezterm.nerdfonts.md_powershell .. " Powershell",
+            label = wezterm.nerdfonts.md_powershell .. "  Powershell",
             args = { "pwsh.exe", "-NoLogo" },
             domain = { DomainName = "local" },
         })
         table.insert(entries, {
-            label = wezterm.nerdfonts.md_shield_crown .. " Powershell (Admin)",
+            label = wezterm.nerdfonts.md_shield_crown .. "  Powershell (Admin)",
             args = { "sudo.exe", "pwsh.exe", "-NoLogo" },
             domain = { DomainName = "local" },
         })
     else
         table.insert(entries, {
-            label = wezterm.nerdfonts.dev_terminal .. " zsh",
+            label = wezterm.nerdfonts.dev_terminal .. "  zsh",
             args = { "zsh", "-l" },
             domain = { DomainName = "local" },
         })
         table.insert(entries, {
-            label = wezterm.nerdfonts.dev_terminal .. " bash",
+            label = wezterm.nerdfonts.dev_terminal .. "  bash",
             args = { "bash", "-l" },
             domain = { DomainName = "local" },
         })
@@ -59,13 +94,23 @@ function M.get_launch_menu(pane)
 
     for _, ssh_host in ipairs(M.get_ssh_hosts()) do
         table.insert(entries, {
-            label = wezterm.nerdfonts.fa_server .. " " .. ssh_host.hostname,
+            label = wezterm.nerdfonts.fa_server .. "  " .. ssh_host.hostname,
             args = {},
             domain = { DomainName = ssh_host.domain_name },
         })
     end
 
     return entries
+end
+
+local function description(type)
+    if type == "tab" then
+        return "New Tab: "
+    elseif type == "horizontal" then
+        return "Horizontal Split: "
+    elseif type == "vertical" then
+        return "Vertical Split: "
+    end
 end
 
 function M.create_selector(type, pane)
@@ -75,14 +120,16 @@ function M.create_selector(type, pane)
     for i, entry in ipairs(entries) do
         table.insert(choices, {
             id = tostring(i),
-            label = string.format("%3i %s", i, entry.label),
+            label = string.format("%s", entry.label),
         })
     end
 
     return {
-        title = type == "horizontal" and "Split Horizontal" or "Split Vertical",
+        title = description(type),
+        description = "",
+        fuzzy_description = "Search:",
         choices = choices,
-        fuzzy = true,
+        fuzzy = false,
         action = wezterm.action_callback(function(window, pane, id)
             local entry = entries[tonumber(id)]
             if entry then

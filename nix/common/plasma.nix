c009565ecee304c45a6d0dcb9013393deca9e1d9
Bart Dorsey <bart@bartdorsey.com>
2026-03-25 17:20:26 -0500

    updating plasma

--- nix/common/plasma.nix
+++ nix/common/plasma.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}: {
   # Enable the SDDM login Manager
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    wayland.enable = true;
+    enable = true;
+  };
 
   # Enable the KDE Plasma desktop environment
   services.desktopManager = {
@@ -15,6 +18,24 @@
   services.system76-scheduler.enable = true;
 
   environment.systemPackages = with pkgs; [
+    # KDE Utilities
+    kdePackages.discover # Optional: Software center for Flatpaks/firmware updates
+    kdePackages.kcalc # Calculator
+    kdePackages.kcharselect # Character map
+    kdePackages.kclock # Clock app
+    kdePackages.kcolorchooser # Color picker
+    kdePackages.kolourpaint # Simple paint program
+    kdePackages.ksystemlog # System log viewer
+    kdePackages.sddm-kcm # SDDM configuration module
+    kdiff3 # File/directory comparison tool
+
+    # Hardware/System Utilities (Optional)
+    kdePackages.isoimagewriter # Write hybrid ISOs to USB
+    kdePackages.partitionmanager # Disk and partition management
+    hardinfo2 # System benchmarks and hardware info
+    wayland-utils # Wayland diagnostic tools
+    wl-clipboard # Wayland copy/paste support
+    vlc # Media player
     quick-webapps
   ];
 

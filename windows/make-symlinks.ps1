857bed4db6ec4b13bba88fb93b7faa9b169588f4
Bart Dorsey <bart@bartdorsey.com>
2026-03-30 13:14:24 -0500

    Adding bash to symlinks

--- windows/make-symlinks.ps1
+++ windows/make-symlinks.ps1
@@ -22,6 +22,7 @@ New-Link $HOME/.dotfiles/glzr/.glzr $HOME/.glzr
 New-Link $HOME/.dotfiles/flameshot/.config/flameshot/flameshot.ini $env:APPDATA/flameshot/flameshot.ini
 New-Link $HOME/.dotfiles/emacs/.config/emacs $HOME/.config/emacs
 New-Link $HOME/.dotfiles/windows/resticprofile/ $env:APPDATA/resticprofile
+New-Link $HOME/.dotfiles/bash/.config/bash $HOME/.config/bash
 # Zen
 New-Link $HOME/.dotfiles/zen/.config/zen/prefs.js $HOME/.config/zen/prefs.js
 New-Link $HOME/.dotfiles/zen/.config/zen/user.js $HOME/.config/zen/user.js

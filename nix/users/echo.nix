97ccc24ba681c8fe5b4d7dee8f289539c80c2e8a
Bart Dorsey <bart@bartdorsey.com>
2026-04-13 16:07:02 -0500

    Fixing group

--- nix/users/echo.nix
+++ nix/users/echo.nix
@@ -15,18 +15,17 @@
     package = pkgs-unstable.neovim-unwrapped;
   };
 
+  users.groups.echo = {
+    gid = 1000;
+  };
+
   users.users.echo = {
+    group = "echo";
     shell = pkgs.zsh;
     isNormalUser = true;
     description = "Bart Dorsey";
-    extraGroups = ["networkmanager" "wheel" "podman" "storage"];
+    extraGroups = ["users" "networkmanager" "wheel" "podman" "storage"];
     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIQ13RJl+pe6gTRnmQmK6X0FRPjfereo4rc8oAivFA4A"];
     linger = true;
-  };
-
-  users.groups.echo = {
-    name = "echo";
-    members = ["echo"];
-    gid = 1000;
   };
 }

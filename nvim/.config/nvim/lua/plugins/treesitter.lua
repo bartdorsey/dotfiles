2a76121fbc77c2fba1e14284d562e1e703aab367
Bart Dorsey <bart@bartdorsey.com>
2026-04-24 10:49:54 -0500

    Updating nvim-treesitter for 0.12

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,64 +1,62 @@
 return {
     -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
     dependencies = {
-        "j-hui/fidget.nvim",
-        "windwp/nvim-ts-autotag",
+        "nvim-treesitter/treesitter-parser-registry",
     },
-    event = { "BufRead", "BufNewFile" },
     config = function()
         -- [[ Configure Treesitter ]]
         -- See `:help nvim-treesitter`
         --
 
-        require("nvim-treesitter.install").prefer_git = false
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {},
-            modules = {},
-            ignore_install = {},
-            autotag = {
-                enable = true,
-            },
-            sync_install = false,
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
+        -- require("nvim-treesitter.configs").setup({
+        --     ensure_installed = {},
+        --     modules = {},
+        --     ignore_install = {},
+        --     autotag = {
+        --         enable = true,
+        --     },
+        --     sync_install = false,
+        --     auto_install = true,
+        --     highlight = { enable = true },
+        --     indent = { enable = true },
+        -- })
 
-        local parser_config =
-            require("nvim-treesitter.parsers").get_parser_configs()
-        parser_config.gotmpl = {
-            install_info = {
-                url = "https://github.com/ngalaiko/tree-sitter-go-template",
-                files = { "src/parser.c" },
-            },
-            filetype = "gotmpl",
-            used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl" },
-        }
+        -- local parser_config =
+        --     require("nvim-treesitter.parsers").get_parser_configs()
+        -- parser_config.gotmpl = {
+        --     install_info = {
+        --         url = "https://github.com/ngalaiko/tree-sitter-go-template",
+        --         files = { "src/parser.c" },
+        --     },
+        --     filetype = "gotmpl",
+        --     used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl" },
+        -- }
 
-        -- Custom treesitter injections
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "TSModuleInstalled",
-            callback = function(args)
-                if args.data == "python" then
-                    vim.treesitter.query.set(
-                        "python",
-                        "injections",
-                        [[ (call function:
-                    (attribute
-                        object: (identifier)
-                        attribute: (identifier) @method
-                            (#eq? @method "execute"))
-                    arguments:
-                    (argument_list
-                        (string
-                            (string_content) @injection.content
-                                (#set! injection.language "sql"))))
-                    ]]
-                    )
-                end
-            end,
-        })
+        -- -- Custom treesitter injections
+        -- vim.api.nvim_create_autocmd("User", {
+        --     pattern = "TSModuleInstalled",
+        --     callback = function(args)
+        --         if args.data == "python" then
+        --             vim.treesitter.query.set(
+        --                 "python",
+        --                 "injections",
+        --                 [[ (call function:
+        --             (attribute
+        --                 object: (identifier)
+        --                 attribute: (identifier) @method
+        --                     (#eq? @method "execute"))
+        --             arguments:
+        --             (argument_list
+        --                 (string
+        --                     (string_content) @injection.content
+        --                         (#set! injection.language "sql"))))
+        --             ]]
+        --             )
+        --         end
+        --     end,
+        -- })
     end,
 }
